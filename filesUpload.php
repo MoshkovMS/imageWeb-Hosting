c58507ddf449f41724d6fb1226f6c6f27b71de68
MoshkovMS <soresmail@gmail.com>
2015-10-29 19:56:30 +0200

    Версія 1.2
    Реалізовано функцію додавання коментаря опису до зображення або
    зображешь, що додаються. Впорядковано код розмітки і програмний код
    HTML, PHP, CSS, JS.

--- filesUpload.php
+++ filesUpload.php
@@ -1,49 +1,78 @@
 <?php
-$dataBaseLink = mysql_connect('localhost', 'root', '');
-mysql_select_db("image",$dataBaseLink);
-$imageLinks = "";
-function generateRandomString($length)
-	{
-	$chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-	$numChars = strlen($chars);
-	$resultString = '';
-	for($i = 0; $i < $length; $i++)
-	{
-	$string .= substr($chars, rand(1, $numChars)-1,1);
-	}
-	return $string;
-	}
-for($i = 0; $i < count($_FILES['fileList']['tmp_name']); $i++)	
-{
-$fileFormat = explode('.',$_FILES['fileList']['name'][$i]);
-if(($fileFormat[count($fileFormat)-1] == 'jpg') || ($fileFormat[count($fileFormat)-1] == 'jpeg'))
-{
-if(is_uploaded_file($_FILES['fileList']['tmp_name'][$i]))
-{
-$adress = generateRandomString(20).'.jpg';
-move_uploaded_file($_FILES['fileList']['tmp_name'][$i], 'images/'.$adress);
-mysql_query('INSERT INTO images SET link = "'.$adress.'", subscribe = '.'"empty"');
-$imageLinks = $imageLinks.$adress.'|'; 
-}		
-}
-}			
+	$dataBaseLink = mysql_connect('localhost', 'root', '');
+	mysql_select_db("image",$dataBaseLink);
+	$imageLinks = "";
+	$description = $_GET['description'];
+	function generateRandomString($length)
+		{
+		$chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+		$numChars = strlen($chars);
+		$resultString = '';
+		for($i = 0; $i < $length; $i++)
+			{
+			$string .= substr($chars, rand(1, $numChars)-1,1);
+			}
+		return $string;
+		}
+	for($i = 0; $i < count($_FILES['fileList']['tmp_name']); $i++)	
+		{
+		$fileFormat = explode('.',$_FILES['fileList']['name'][$i]);
+		if(($fileFormat[count($fileFormat)-1] == 'jpg') || ($fileFormat[count($fileFormat)-1] == 'jpeg'))
+			{
+			if(is_uploaded_file($_FILES['fileList']['tmp_name'][$i]))
+				{
+				$adress = generateRandomString(20).'.jpg';
+				move_uploaded_file($_FILES['fileList']['tmp_name'][$i], 'images/'.$adress);
+				if($description == '')$description = 'empty';
+				mysql_query('INSERT INTO images SET link = "'.$adress.'", subscribe = '.'"'.$description.'"');
+				$imageLinks = $imageLinks.$description.'|'.$adress.'|'; 
+				}		
+			}
+		}			
 ?>
 <!DOCTYPE html>
 <html>
 	<head>
+		<style>
+			#imageDescription
+				{
+				background: #ffffff;
+				border-radius: 5px;
+				border: black solid 1px;
+				overflow: auto;
+				}
+		</style>
 	</head>
 	<body>
 		<script>
-		document.getElementsByTagName('body')[0].innerHTML = '<form  action = "filesUpload.php" method = "post" enctype = "multipart/form-data"><input name = "fileList[]" type = "file" multiple = "true" min = "1" max = "10"><input type = "submit" value = "Відправити"></form>';
-		top.getMyPhotos();	
+			function setDescription()
+					{
+					description = document.getElementById('imageDescription').value;
+					document.getElementsByTagName('form')[0].setAttribute('action', 'filesUpload.php?description='+description);
+					description = '';
+					}
+			document.getElementsByTagName('body')[0].innerHTML = '<form  action = "" method = "post" enctype = "multipart/form-data"><input name = "fileList[]" type = "file" multiple = "true" min = "1" max = "10"><input type = "submit" value = "Відправити"></form>';
+			top.getMyPhotos();		
 		</script>
+		<table>
+			<tr>
+				<td>
+					Ваш коментар до зображення, або ключове слово пошуку:
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<textarea rows = 2 cols = 44 id = "imageDescription" onkeyup = "setDescription();">
+					</textarea>
+				</td>
+			</tr>
+		</table>
 	<?php
-	
-	$imageLinksArr = explode('|',$imageLinks);
-	if(count($imageLinksArr) > 1)
-	{
-	echo'<script>top.setNewImages(\''.$imageLinks.'\')</script>';
-	}
+		$imageLinksArr = explode('|',$imageLinks);
+		if(count($imageLinksArr) > 2)
+			{
+			echo'<script>top.setNewImages(\''.$imageLinks.'\')</script>';
+			}
 	?>
 	</body>
 </html>
--- index.php
+++ index.php
@@ -3,64 +3,65 @@
 		<title>
 			Миттєвий хостинг зображень
 		</title>
-	<style>
-		iframe
-			{
-			border: 0px;
-			background: #aaaaaa;
-			margin-top: 10px;
-			margin-bottom: 10px;
-			border-radius: 5px;
-			box-shadow: 0px 0px 10px 1px;
-			}
-		img
-			{
-			display: block;
-			max-width: 100%;
-			max-height: 100%;
-			}
-		#imagesBlock
-			{
-			margin: auto;
-			width: 550px;
-			background: #ffffff;
-			border-radius: 5px;
-			box-shadow: 0px 0px 10px 1px;
-			}
-		#content
-			{
-			box-shadow: 0px 0px 10px 1px;
-			text-align: center;
-			width: 700px;
-			background: #ff0000;
-			margin: auto;
-			border: black solid 1px;
-			background-image: url("back.jpg");
-			width: 650px;
-			border-radius: 10px;
-			padding-bottom: 5px;
-			}
-	</style>
+		<style>
+			iframe
+				{
+				border: 0px;
+				background: #aaaaaa;
+				margin-top: 10px;
+				margin-bottom: 10px;
+				border-radius: 5px;
+				box-shadow: 0px 0px 10px 1px;
+				}
+			img
+				{
+				display: block;
+				max-width: 100%;
+				max-height: 100%;
+				}
+			#imagesBlock
+				{
+				margin: auto;
+				width: 550px;
+				background: #ffffff;
+				border-radius: 5px;
+				box-shadow: 0px 0px 10px 1px;
+				}
+			#content
+				{
+				box-shadow: 0px 0px 10px 1px;
+				text-align: center;
+				width: 700px;
+				background: #ff0000;
+				margin: auto;
+				border: black solid 1px;
+				background-image: url("back.jpg");
+				width: 650px;
+				border-radius: 10px;
+				padding-bottom: 5px;
+				}
+		</style>
 	</head>
 	<body>
-	<div id = "content">
-		<iframe src = "filesUpload.php" height = "35" width = "350" scrolling = "no" id = "iFameWin">
-		</iframe>
-		<div id = "imagesBlock">
+		<div id = "content">
+			<iframe src = "filesUpload.php" height = "120" width = "350" scrolling = "no" id = "iFameWin">
+			</iframe>
+			<div id = "imagesBlock">
+			</div>
 		</div>
-	</div>
-	<script>
-		function setNewImages(imagesArr)
-			{
-			document.getElementById("imagesBlock").innerHTML = '';
-			adressArr = imagesArr.split('|');
-			for(i = 0; (i < adressArr.length) && (adressArr[i].length > 1); i++)
+		<script>
+			function setNewImages(imagesArr)
 				{
-				image = '<a href = "http://localhost/images/'+adressArr[i]+'" target = "_blank"><img src  = "images/'+adressArr[i]+'">'+"</a>"
-				link = 'Ваше посилання: http://localhost/images/'+adressArr[i];
-				document.getElementById("imagesBlock").innerHTML += image+link+"<br>";
+				document.getElementById("imagesBlock").innerHTML = '';
+				adressArr = imagesArr.split('|');
+				for(i = 0; (i < adressArr.length) && (adressArr[i+1].length > 2); i+=2)
+					{
+					image = '<a href = "http://localhost/images/'+adressArr[i+1]+'" target = "_blank"><img src  = "images/'+adressArr[i+1]+'">'+"</a>"
+					desc = 'Ваш опис зображення: '+adressArr[i]+'<br>';
+					link = 'Ваше посилання: http://localhost/images/'+adressArr[i+1];
+					document.getElementById("imagesBlock").innerHTML += image+'<hr>'+desc+'<hr>'+link+'<hr>';
+					}
 				}
-			}
-	</script>
+		</script>
 	</body>
 </html>
